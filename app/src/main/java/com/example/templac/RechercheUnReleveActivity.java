c6c5517aefc1efdb55508508539eb7d52fa0c153
polaurelien <pcarsin@la-joliverie.com>
2020-10-12 15:40:20 +0200

    Création activity_rechercher_un_releve.xml et RechercheUnReleveActivity.java et mise à jour d'Android Manifest

---
+++ app/src/main/java/com/example/templac/RechercheUnReleveActivity.java
@@ -0,0 +1,12 @@
+package com.example.templac;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RechercheUnReleveActivity extends Activity
+{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rechercher_un_releve);
+    }
+}
