7fb2b0bd35233d4b1399a70e119bcf2889207151
polaurelien <pcarsin@la-joliverie.com>
2020-10-14 10:02:18 +0200

    Création de SaisirUnReleveActivity

---
+++ app/src/main/java/com/example/templac/AfficherUnReleve.java
@@ -0,0 +1,8 @@
+package com.example.templac;
+
+import android.app.Activity;
+
+public class AfficherUnReleve extends Activity
+{
+    //A COMPLETER
+}
--- app/src/main/java/com/example/templac/SaisirUnReleveActivity.java
+++ app/src/main/java/com/example/templac/SaisirUnReleveActivity.java
@@ -1,11 +1,25 @@
 package com.example.templac;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SaisirUnReleveActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saisir_un_releve);
+
+        Button btnQuitter = findViewById(R.id.btnQuitterSaisir);
+        View.OnClickListener ecouteur1 = new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(SaisirUnReleveActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        };
+        btnQuitter.setOnClickListener(ecouteur1);
     }
 }
