491e570e0965dec3af9f459ea0d416eaa13bfad2
Salim <thaichai4433@gmail.com>
2020-10-14 09:46:35 +0200

    Interface Saisir un relevé associer à la page d'acceuil

--- app/src/main/java/com/example/templac/MainActivity.java
+++ app/src/main/java/com/example/templac/MainActivity.java
@@ -2,14 +2,42 @@ package com.example.templac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Button btnAfficherUnReleve = findViewById(R.id.btnRecherche);
+        View.OnClickListener ecouteur1 = new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, RechercheUnReleveActivity.class);
+                startActivity(intent);
+
+            }
+
+        };
+        btnAfficherUnReleve.setOnClickListener(ecouteur1);
+
+        Button btnSaisie = findViewById(R.id.btnSaisie);
+        View.OnClickListener ecouteur2 = new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, SaisirUnReleveActivity.class);
+                startActivity(intent);
+
+            }
+
+        };
+        btnSaisie.setOnClickListener(ecouteur2);
     }
 }
---
+++ app/src/main/java/com/example/templac/SaisirUnReleveActivity.java
@@ -0,0 +1,11 @@
+package com.example.templac;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SaisirUnReleveActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saisir_un_releve);
+    }
+}
