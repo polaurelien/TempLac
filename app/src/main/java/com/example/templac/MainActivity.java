718fcf16642f8775bfb74741700d56363fe85864
polaurelien <pcarsin@la-joliverie.com>
2020-10-14 09:19:41 +0200

    Code des boutons pour la recherche d'un releve

--- app/src/main/java/com/example/templac/MainActivity.java
+++ app/src/main/java/com/example/templac/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.templac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Button btnAfficherUnReleve = findViewById(R.id.btnRecherche);
+        View.OnClickListener ecouteur1 = new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, RechercheUnReleveActivity.class);
+                startActivity(intent);
+            }
+        };
+        btnAfficherUnReleve.setOnClickListener(ecouteur1);
     }
 }
