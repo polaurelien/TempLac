e447fccdf09eee1efd357684950595c69d51447d
Salim <thaichai4433@gmail.com>
2020-11-04 09:25:03 +0100

    version sans createbdd completer

--- app/src/main/java/com/example/templac/CreateBDD.java
+++ app/src/main/java/com/example/templac/CreateBDD.java
@@ -4,15 +4,14 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.sql.Date;
+
 
 public class CreateBDD extends SQLiteOpenHelper
-{
-    public CreateBDD(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)
-    {
-        super(context,name,factory, version);
-    }
 
+{
+    public CreateBDD( Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
     //TABLE LAC ------------------------------------------------------------------------------------
 
@@ -21,30 +20,17 @@ public class CreateBDD extends SQLiteOpenHelper
     private static final String COL_NOM = "nomLac";
     private static final String COL_LONGITUDE = "longitudeLac";
     private static final String COL_LATITUDE = "latitudeLac";
-    private static final String CREATE_TABLE_LAC = "CREATE TABLE " + TABLE_LAC + " ("+COL_ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+ COL_NOM + " TEXT NOT NULL, " + COL_LONGITUDE + " TEXT NOT NULL, " + COL_LATITUDE + " TEXT NOT NULL);";
+    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_LAC + " ("+COL_ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+ COL_NOM + " TEXT NOT NULL, " + COL_LONGITUDE + " TEXT NOT NULL, " + COL_LATITUDE + " TEXT NOT NULL);";
 
-    //TABLE RELEVE ---------------------------------------------------------------------------------
-
-    private static final String TABLE_RELEVE = "treleve";
-    private static final String COL_LAC = "idLac";
-    private static final String COL_dateReleve = "dateReleve";
-    private static final String COL_heureReleve = "heureReleve";
-    private static final String COL_tempReleve = "tempReleve";
-    private static final String CREATE_TABLE_RELEVE = "CREATE TABLE " + TABLE_RELEVE + " ("+COL_ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+ COL_LAC + " TEXT NOT NULL, " + COL_dateReleve + " TEXT NOT NULL, " + COL_heureReleve+ " TEXT NOT NULL, " + COL_tempReleve + " TEXT NOT NULL);";
 
 
     @Override
-    public void onCreate(SQLiteDatabase db)
-    {
-        db.execSQL(CREATE_TABLE_LAC);
-        db.execSQL(CREATE_TABLE_RELEVE);
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldversion, int newversion)
-    {
-        db.execSQL("DROP TABLE " + TABLE_LAC + ";");
-        db.execSQL("DROP TABLE " + TABLE_RELEVE + ";");
-        onCreate(db);
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
     }
 }
