02dc5eeccb0c941b6018d12fbf43fec2c45da106
polaurelien <pcarsin@la-joliverie.com>
2020-11-04 10:46:02 +0100

    Fin création BDD

--- app/src/main/java/com/example/templac/SaisirUnReleveActivity.java
+++ app/src/main/java/com/example/templac/SaisirUnReleveActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.sql.Date;
 
@@ -16,10 +19,39 @@ public class SaisirUnReleveActivity extends Activity {
         setContentView(R.layout.activity_saisir_un_releve);
         final DAOBDD bdd = new DAOBDD(this);
         bdd.open();
+        final String[] lheure = new String[1];
         final EditText nom = findViewById(R.id.editTextNomLac);
         final EditText date = findViewById(R.id.editTextDate);
         final EditText temp = findViewById(R.id.editTextSaisirTemp);
         final Spinner heure = (Spinner) findViewById(R.id.ListeHeureSaisir);
+        final String[] lesHeures = {"00h", "6h", "12h", "18h"};
+
+        ArrayAdapter<String> dataAdapterR = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lesHeures);
+        dataAdapterR.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        heure.setAdapter(dataAdapterR);
+        heure.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                lheure[0] = String.valueOf(heure.getSelectedItem());
+                Toast.makeText(SaisirUnReleveActivity.this, "Vous avez choisie : " + "\nle compteur numéro : " + lheure[0], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        /*ArrayAdapter<String> dataAdaptaterR = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, lesHeures);
+        dataAdaptaterR.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        heure.setAdapter(dataAdaptaterR);
+        heure.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                lesHeures[0] = String.valueOf(heure.getSelectedItem());
+            }
+
+            public void onNothingSelected(AdapterView<?> adapterView)
+            {}
+        });*/
 
         Button btnQuitter = findViewById(R.id.btnQuitterSaisir);
         View.OnClickListener ecouteur1 = new View.OnClickListener() {
